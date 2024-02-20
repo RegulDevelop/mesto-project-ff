a286740742b68bdc427d2b12e7930bb97e58f9cb
Anton Tyurin <regul.t.86@gmail.com>
2024-02-20 18:01:44 +0600

    Проект Mesto js - 1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placeList = document.querySelector('.places__list');
+
+function createCard(data) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = data.link;
+  cardElement.querySelector('.card__title').textContent = data.name;
+
+  deleteButton.addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+initialCards.forEach((element) => {
+  const cardElement = createCard(element, deleteCard);
+
+  placeList.append(cardElement);
+});
